ed9ad87cc704f8a1ccf738ad7aa10aa5ed73e4b4
Jon Morehouse <morehousej09@gmail.com>
2013-02-27 16:25:18 -0800

    Fixed retail navigation

--- application/views/navigation/navigation_retail.php
+++ application/views/navigation/navigation_retail.php
@@ -3,33 +3,33 @@
 	
 	<li>Type
 		<ul>
-			<li><a href='<?php echo site_url("property/{$this->id}/type/rent");?>'>Rent</a></li>
-			<li><a href='<?php echo site_url("property/{$this->id}/type/buy");?>'>Buy</a></li>
+			<li><a href='<?php echo site_url("browse/{$this->id}/type/rent");?>'>Rent</a></li>
+			<li><a href='<?php echo site_url("browse/{$this->id}/type/buy");?>'>Buy</a></li>
 		</ul>
 	</li>
 	
 	<li>Rental Price
 		<ul>
-			<li><a href='<?php echo site_url("property/{$this->id}/rent_price/over_1000");?>'>Over $1000</a></li>
-			<li><a href='<?php echo site_url("property/{$this->id}/rent_price/under_1000");?>'>Under $1000</a></li>
+			<li><a href='<?php echo site_url("browse/{$this->id}/rent_price/over_1000");?>'>Over $1000</a></li>
+			<li><a href='<?php echo site_url("browse/{$this->id}/rent_price/under_1000");?>'>Under $1000</a></li>
 		</ul>
 	</li>
 	
 	<li>Location
 		<ul>
-			<li><a href='<?php echo site_url("property/{$this->id}/location_category/okanagan_valley");?>'>Okanagan Valley: Kamloops and Kelowna</a></li>
-			<li><a href='<?php echo site_url("property/{$this->id}/location_category/richmond_delta");?>'>Richmond/Delta</a></li>
-			<li><a href='<?php echo site_url("property/{$this->id}/location_category/fraser_valley");?>'>Fraser Valley: Abbotsford, Mission</a></li>	
-			<li><a href='<?php echo site_url("property/{$this->id}/location_category/squamish");?>'>Squamish</a></li>
-			<li><a href='<?php echo site_url("property/{$this->id}/location_category/vancouver_island");?>'>Vancouver Island: Parksville</a></li>
-			<li><a href='<?php echo site_url("property/{$this->id}/location_category/vancouver");?>'>Vancouver</a></li>
-			<li><a href='<?php echo site_url("property/{$this->id}/location_category/burnaby");?>'>Burnaby</a></li>
-			<li><a href='<?php echo site_url("property/{$this->id}/location_category/pitt_meadows");?>'>Pitt Meadows</a></li>
+			<li><a href='<?php echo site_url("browse/{$this->id}/location_category/okanagan_valley");?>'>Okanagan Valley: Kamloops and Kelowna</a></li>
+			<li><a href='<?php echo site_url("browse/{$this->id}/location_category/richmond_delta");?>'>Richmond/Delta</a></li>
+			<li><a href='<?php echo site_url("browse/{$this->id}/location_category/fraser_valley");?>'>Fraser Valley: Abbotsford, Mission</a></li>	
+			<li><a href='<?php echo site_url("browse/{$this->id}/location_category/squamish");?>'>Squamish</a></li>
+			<li><a href='<?php echo site_url("browse/{$this->id}/location_category/vancouver_island");?>'>Vancouver Island: Parksville</a></li>
+			<li><a href='<?php echo site_url("browse/{$this->id}/location_category/vancouver");?>'>Vancouver</a></li>
+			<li><a href='<?php echo site_url("browse/{$this->id}/location_category/burnaby");?>'>Burnaby</a></li>
+			<li><a href='<?php echo site_url("browse/{$this->id}/location_category/pitt_meadows");?>'>Pitt Meadows</a></li>
 		</ul>
 	</li>
 	
 
-	<li><a href='<?php echo site_url("property/{$this->id}/all");?>'>All</a></li>
+	<li><a href='<?php echo site_url("browse/{$this->id}/all");?>'>All</a></li>
 </div>
 
 
