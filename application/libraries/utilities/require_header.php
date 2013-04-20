44a0faae60f2c348fe2164d36c82b6caa5d987d1
Jon Morehouse <morehousej09@gmail.com>
2013-04-20 02:07:24 -0700

    Initialize require_header

---
+++ application/libraries/utilities/require_header.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+
+class Require_header extends Dynamic_header {
+
+	public function Require_header($parameters) {
+
+		
+		parent::__construct($parameters);
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
