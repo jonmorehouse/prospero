79d8d2ee5a008d171cbb6cbaa80c339f87963d65
Jon Morehouse <morehousej09@gmail.com>
2013-04-23 02:50:20 -0700

    Change search header

--- application/views/navigation/search.php
+++ application/views/navigation/search.php
@@ -1,7 +1,7 @@
 <div id='search'>
 	<form action='<?php echo site_url('browse/search/');?>' method='post'>
 		<input type='text' value='<?php echo (isset($this->search_value)? $this->search_value : 
-		'Search via name, location, price etc...'); ?>' name='search' />
+		'Search'); ?>' name='search' />
 		
 		<input type='image' name='submit' src='<?php echo base_url('resources/images/site_wide/search.png');?>' />
 	</form>
