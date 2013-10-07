a1dc98229e26131acd230294e52b35b044081ed3
Jon Morehouse <morehousej09@gmail.com>
2013-10-07 00:16:23 -0700

    Rebuild logo button css for better hover experience

--- application/views/site_wide/logo.php
+++ application/views/site_wide/logo.php
@@ -1,5 +1,5 @@
 <div id='logo'>
 	<a href='<?php echo $this->logo['link'];?>'>
-	<img alt='<?php echo $this->logo['alt'];?>' src='<?php echo $this->logo['url'];?>' />
+		<img alt='<?php echo $this->logo['alt'];?>' src='<?php echo $this->logo['url'];?>' />
 	</a>
 </div>
